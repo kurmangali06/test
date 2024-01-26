38893725663be8de9789e7d948aa7bb79b77004d
Kurman <kurmangali0606@gmail.com>
2024-01-26 12:09:21 +0600

    feat: add 404 page

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,11 +1,40 @@
-<template >
-    <div>
-        404
+<template>
+    <div class="page-404">
+      <h1>404</h1>
+      <p>Страница не найдена</p>
+      <router-link to="/">Вернуться на главную</router-link>
     </div>
-</template>
-<script setup lang="ts">
+  </template>
+  
+<style lang="scss" scoped>
+  .page-404 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    text-align: center;
 
-</script>
-<style lang="">
-    
+    h1 {
+      font-size: 5rem;
+      font-weight: bold;
+    }
+
+    p {
+      font-size: 1.5rem;
+      margin: 20px 0;
+    }
+
+    a {
+      color: #007bff;
+      font-size: 1.2rem;
+      text-decoration: none;
+      border-bottom: 1px solid transparent;
+      transition: border-bottom-color 0.3s;
+
+      &:hover {
+        border-bottom-color: currentColor;
+      }
+    }
+  }
 </style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -2,17 +2,19 @@ import type { RouteRecordRaw } from "vue-router";
 
 export const routes: RouteRecordRaw[] = [
   {
+    path: "/",
+    redirect: '/margarita',
+    children: [
+      {
+        path: "/:cocktail",
+        name: 'product',
+        component: () => import("../pages/Product.vue"),
+      },
+    ]
+  },
+  {
     path: "/:pathMatch(.*)*",
     name: "PageNotFound",
     component: () => import("../components/404.vue"),
-  },
-  {
-    path: "/",
-    redirect: '/margarita',
-  },
-  {
-    path: "/:cocktail",
-    name: 'product',
-    component: () => import("../pages/Product.vue"),
   },
 ];
